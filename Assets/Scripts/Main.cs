8b7d12c55a001ee78393ce645f483d353327ab89
Okazari <plouzennec.benjamin@gmail.com>
2019-08-02 14:58:02 +0200

    :tada: Working example with code based animated sprite :D

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+
+  public Player me;
+
+  public Transform tr;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    me = new Player("bigunit", new Vector2(2, 2));
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+}
